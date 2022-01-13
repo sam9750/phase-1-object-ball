855dc7bb4014fc4df2ca63bc9b6b24bbe790e1ce
sam9750 <sam.wadhwa95@gmail.com>
2022-01-13 10:31:35 -0500

    Draft

---
+++ src/00-objectball.js
@@ -0,0 +1,43 @@
+function gameObject();
+const home = {
+   
+    teamName: "Brooklyn Nets"
+    colors: [black, white]
+    players{ 
+        names: 
+        "Alan Anderson": {...},
+        "Reggie Evans": {...}, 
+        "Brook Lopez" : {...},
+        "Mason Plumlee" : {...},
+        "Jason Terry" : {...},
+    }
+
+
+    },
+    const away = 
+    colors: [turquoise, purple],
+    teamName: "Charlotte Hornets"
+    players: {Jeff Adrien	Bismak Biyombo	DeSagna Diop	Ben Gordon	Brendan Haywood
+
+    
+  } 
+  console.log(gameObject());
+}
+
+
+console.log(gameObject());
+/*
+{
+  home: {
+    teamName: "",
+    colors: [...],
+    players: {
+      "Alan Anderson": {...},
+      "Reggie Evans": {...}
+    }
+  },
+  away: {
+    ...
+  } 
+}
+*/
